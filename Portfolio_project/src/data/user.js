98104e34d428c2972d87c03239eef282c968e22c
Brian Escobar <70177287+Brian-Escobar@users.noreply.github.com>
2024-05-17 23:22:52 -0700

    Final touches
    Final touches

---
+++ Portfolio_project/src/data/user.js
@@ -0,0 +1,57 @@
+const INFO = {
+	main: {
+		title: "Brian's Super Awesome Portfolio",
+		name: "Brian Escobar",
+		email: "brianescobar523@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Brian-Escobar",
+		linkedin: "https://linkedin.com/brian-escobar-b495a8289",
+		instagram: "https://instagram.com/brianquartz/",
+		
+	},
+
+	homepage: {
+		title: "Hi Welcome to My Portfolio!",
+		description:
+			"Hi, My name is Brian Escobar and I am a soon to be graduate of Cal State Fullerton with a Bachelors in Computer Science. I am proficient at programming in C++, Python, and Swift. ",
+	},
+
+	about: {
+		title: "Nice to meet you, I'm Brian Escobar.",
+		description:
+			"Here you can find all of my contact information as well as a link to my GitHub where I showcase my finished projects.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1 - At a Glance",
+			description:
+				"An ios Application made in Xcode using Swift.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/swift/swift.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2 - Compiler Project",
+			description:
+				"A program that replicates the process of a compiler for an imaginary programming language called 'Rat23F'.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		
+	],
+};
+
+export default INFO;
